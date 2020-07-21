ad18f8f7b3691fbbe66eaa26bd3c55f4259e864d
djo95 <45712968+djo95@users.noreply.github.com>
2020-07-21 02:41:16 +0100

    add NL project

---
+++ Beautysane/Webapps/NLweb/src/App.js
@@ -0,0 +1,98 @@
+import React, {Component} from 'react';
+import {BrowserRouter as Router, Route} from "react-router-dom";
+import "./assets/css/feather.css"
+import "./assets/css/materialdesignicons.css"
+import "./assets/css/dripiIcons.css"
+import "./assets/css/fa.css"
+import "./assets/css/fonts.css"
+import './assets/scss/DefaultTheme.scss';
+import './App.css'
+
+
+
+
+
+
+
+
+
+
+import firebase from "firebase/app";
+
+import listeRecette from "./pages/listRecette/listeRecette";
+import recette from "./pages/listRecette/recette";
+import questions from "./pages/questions/questions";
+import addRecette from "./pages/addRecette/addRecette";
+import  authentification from "./pages/auth/authentification";
+
+
+
+
+
+
+
+const firebaseConfig =  {
+    apiKey: "AIzaSyADBksHxor-OBDNMwhLs8Z2-dikj8YTvTM",
+    authDomain: "beautysane-61cf2.firebaseapp.com",
+    databaseURL: "https://beautysane-61cf2.firebaseio.com",
+    projectId: "beautysane-61cf2",
+    storageBucket: "beautysane-61cf2.appspot.com",
+    messagingSenderId: "716549886066",
+    appId: "1:716549886066:web:cd3089ae9c371d3245f2eb",
+    measurementId: "G-MM0W4MPEX2"
+};
+
+
+
+firebase.initializeApp(firebaseConfig);
+
+class App extends Component{
+
+
+  componentWillMount() {
+
+  }
+
+
+    render() {
+
+    return(
+        <Router>
+
+
+
+            <Route exact path="/" component={listeRecette}/>
+            <Route exact path="/questions" component={questions}/>
+            <Route exact path="/addRecette" component={addRecette}/>
+            <Route exact path="/recette/:id" component={recette}/>
+            <Route exact path="/login" component={authentification}/>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            {/*<Route exact path="/login" name="login" component={Login}/>
+          <Route exact path="/logout" name="logout" component={Logout}/>*/}
+
+        </Router>
+    )
+  }
+
+}
+
+export default App;
+
+
+
+
